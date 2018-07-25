fdf75f62eec0af20b54ebb1b387b38bb48f2ac63
Michael Panetta <mtpanetta@gmail.com>
2018-07-25 13:25:01 -0500

    initial setup

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import '../styles/App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Michael Panetta</h1>
+        </header>
+        <div className="body">
+          <h3>More to Come</h3>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
