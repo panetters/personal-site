3183f08f97fbf8074514dbef65bbf7ca0a69aa87
Michael Panetta <mtpanetta@gmail.com>
2018-08-21 14:51:26 -0500

    added link, fixed detail padding

--- src/data/appdata.js
+++ src/data/appdata.js
@@ -1,12 +1,17 @@
 const appdata = [
   {
     name: 'Handycap',
-    detail: 'Golf handicap tracker',
+    detail: 'Golf handicap calculator and tracker',
     link: 'https://handycap.herokuapp.com'
   },
   {
+    name: 'Tweet Map',
+    detail: 'Heatmap of the U.S. based on Twitter keywords',
+    link: 'https://heattweet.herokuapp.com'
+  },
+  {
     name: 'Food Fighter',
-    detail: 'Choose a restaurant with friends',
+    detail: 'Application to choose a restaurant with friends',
     link: 'https://food-fighter.herokuapp.com'
   },
 ];
